4a637a825661b1538f6f1fadc098fda69a81aafe
LuisMonsalve14 <125522763+LuisMonsalve14@users.noreply.github.com>
2026-06-01 00:56:21 -0300

    Implement Telegram message sending functionality
    Send a message to Telegram using bot API.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hola mundo")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+requests.get(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    params={
+        "chat_id": CHAT_ID,
+        "text": "🚀 Mensaje enviado desde GitHub Actions"
+    }
+)
+
+print("Mensaje enviado")
